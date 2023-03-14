9a4aa32d03d300c535222e00b06aefa18d05fe88
Julian Dominguez <JulianDom@outlook.com.ar>
2023-03-14 14:22:09 -0300

    day 1

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <div>
+        <section className='head'>
+            <div className="container d_flex">
+                <div className="left row">
+                    <i className="fa fa-phone"></i>
+                    <label>+54 9 221889938</label>
+                    <i className="fa fa-envelope"></i>
+                    <label>ecommerce@project.com</label>
+                </div>
+                <div className="right row Rtext">
+                    <label > Theme FAQ'S</label>
+                    <label > Need Helps</label>
+                    <label>EN</label>
+                    <span>🏳️</span>
+                    <label>USD</label>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+export default Head
--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import './Header.css'
+import Head from './Head'
+import Navbar from './Navbar'
+import Search from "./Search"
 
-export const Header = () => {
+const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+       <h1> Header </h1>
+       <Head />
+       <Search />
+       <Navbar />
+        </div>
   )
 }
+
+export default Header
